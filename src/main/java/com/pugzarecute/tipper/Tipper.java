f2417b84baaee238122897f47e67e7273120b405
PugzAreCute <me@pugzarecute.com>
2023-02-08 18:20:44 +0530

    Disable enchanting table and add armor weight

--- src/main/java/com/pugzarecute/tipper/Tipper.java
+++ src/main/java/com/pugzarecute/tipper/Tipper.java
@@ -1,12 +1,16 @@
 package com.pugzarecute.tipper;
 
+import com.pugzarecute.tipper.events.ArmorEventSubscriver;
 import com.pugzarecute.tipper.events.VillagerTradeEventSubscriber;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 @Mod("tipper")
 public class Tipper {
+    //UUID LIST
+    // 3f1109ab-edcd-4a86-aed6-a0a3a587bd8a - Armor slowdown
     public Tipper(){
         MinecraftForge.EVENT_BUS.register(new VillagerTradeEventSubscriber());
+        MinecraftForge.EVENT_BUS.register(new ArmorEventSubscriver());
     }
     //Love mixins
 }
---
+++ src/main/java/com/pugzarecute/tipper/events/ArmorEventSubscriver.java
@@ -0,0 +1,45 @@
+package com.pugzarecute.tipper.events;
+
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ArmorEventSubscriver {
+    static AttributeModifier lvl1 = new AttributeModifier("3f1109ab-edcd-4a86-aed6-a0a3a587bd8a", -0.15F, AttributeModifier.Operation.MULTIPLY_TOTAL);
+    static AttributeModifier lvl2 = new AttributeModifier("3f1109ab-edcd-4a86-aed6-a0a3a587bd8a", -0.3F, AttributeModifier.Operation.MULTIPLY_TOTAL);
+
+    @SubscribeEvent
+    public void eventX(LivingEquipmentChangeEvent event) {
+        double armorLevelCorrect = 0;
+        for (ItemStack armorItem: event.getEntity().getArmorSlots()) {
+            for(AttributeModifier ma : armorItem.getAttributeModifiers(EquipmentSlot.HEAD).get(Attributes.ARMOR)){
+                armorLevelCorrect += ma.getAmount();
+            }
+            for(AttributeModifier ma : armorItem.getAttributeModifiers(EquipmentSlot.CHEST).get(Attributes.ARMOR)){
+                armorLevelCorrect += ma.getAmount();
+            }
+            for(AttributeModifier ma : armorItem.getAttributeModifiers(EquipmentSlot.LEGS).get(Attributes.ARMOR)){
+                armorLevelCorrect += ma.getAmount();
+            }
+            for(AttributeModifier ma : armorItem.getAttributeModifiers(EquipmentSlot.FEET).get(Attributes.ARMOR)){
+                armorLevelCorrect += ma.getAmount();
+            }
+        }
+
+        if(armorLevelCorrect >= 12 && armorLevelCorrect <=17){
+            event.getEntityLiving().getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(lvl1);
+            event.getEntityLiving().getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(lvl2);
+            event.getEntityLiving().getAttribute(Attributes.MOVEMENT_SPEED).addPermanentModifier(lvl1);
+        } else if(armorLevelCorrect >= 18 && armorLevelCorrect <=20){
+            event.getEntityLiving().getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(lvl1);
+            event.getEntityLiving().getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(lvl2);
+            event.getEntityLiving().getAttribute(Attributes.MOVEMENT_SPEED).addPermanentModifier(lvl2);
+        }else{
+            event.getEntityLiving().getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(lvl1);
+            event.getEntityLiving().getAttribute(Attributes.MOVEMENT_SPEED).removeModifier(lvl2);
+        }
+    }
+}
