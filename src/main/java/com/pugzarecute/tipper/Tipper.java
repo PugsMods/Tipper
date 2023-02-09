35a7b2ce698784153fbf9d5c1fb507f544c1c19a
PugzAreCute <me@pugzarecute.com>
2023-02-09 17:19:19 +0530

    Re-Enable enchanting table

--- src/main/java/com/pugzarecute/tipper/Tipper.java
+++ src/main/java/com/pugzarecute/tipper/Tipper.java
@@ -1,6 +1,7 @@
 package com.pugzarecute.tipper;
 
 import com.pugzarecute.tipper.events.ArmorEventSubscriber;
+import com.pugzarecute.tipper.events.SkilletDebuffer;
 import com.pugzarecute.tipper.events.VillagerTradeEventSubscriber;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -9,9 +10,11 @@ import net.minecraftforge.fml.common.Mod;
 public class Tipper {
     //UUID LIST
     // 3f1109ab-edcd-4a86-aed6-a0a3a587bd8a - Armor slowdown
+    // 4ff94819-b823-4b72-8f0e-e36e11af9eb3 - Skillet debuf
     public Tipper(){
         MinecraftForge.EVENT_BUS.register(new VillagerTradeEventSubscriber());
         MinecraftForge.EVENT_BUS.register(new ArmorEventSubscriber());
+        MinecraftForge.EVENT_BUS.register(new SkilletDebuffer());
     }
     //Love mixins
 }
